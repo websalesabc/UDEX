50a2421529a0927c18c3bd0ad8a5ded2dec30b6f
Johannes Breytenbach <websalesabc@gmail.com>
2022-11-11 12:25:49 +1000

    2. Smoke Test

---
+++ scripts/boilerplate_deploy.js
@@ -0,0 +1,10 @@
+async function main() {
+  //do stuff
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/sample_deploy.js
+++ scripts/sample_deploy.js
@@ -1,3 +1,16 @@
+async function main() {
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
+
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //

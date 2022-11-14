0e03cb7eb445f51b56286def19534d7cd421b06e
Johannes Breytenbach <websalesabc@gmail.com>
2022-11-14 22:09:00 +1000

    9. Deposit tokens

---
+++ contracts/Exchange.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./Token.sol";
+
+contract Exchange {
+    address public feeAccount;
+    uint256 public feePercent;
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+
+    constructor(address _feeAccount, uint256 _feePercent) {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+    }
+
+//------------------
+// DEPOSIT and WITHDRAW tokens
+function depositToken(address _token, uint256 _amount) public {
+  //Transfer tokens to Exchange
+  require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+  //update balance
+  tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+  //emit event
+  emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
+  }
+
+  //check balances This is a wrapper function
+  function balanceOf(address _token, address _user)
+  public
+  view
+  returns (uint256)
+  {
+    return tokens[_token][_user];
+  }
+
+}
---
+++ test/Exchange.js
@@ -0,0 +1,83 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Exchange', () => {
+  let deployer, feeAccount, exchange
+
+  const feePercent = 10
+
+  beforeEach(async () => {
+    const Exchange = await ethers.getContractFactory('Exchange')
+    const Token = await ethers.getContractFactory('Token')
+
+    token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    feeAccount = accounts[1]
+    user1 = accounts[2]
+
+    let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100))
+
+    exchange = await Exchange.deploy(feeAccount.address, feePercent)
+  })
+
+  describe('Deployment', () => {
+
+    it('tracks the fee account', async () => {
+      expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+    })
+
+    it('tracks the fee percent', async () => {
+      expect(await exchange.feePercent()).to.equal(feePercent)
+    })
+
+  })
+
+  describe ('Depositing tokens', () => {
+    let transaction, result
+    let amount = tokens(10)
+
+
+
+    describe('Success', () => {
+      beforeEach(async () => {
+        //approve tokens
+        transaction = await token1.connect(user1).approve(exchange.address, amount)
+        result = await transaction.wait()
+        //deposit token
+        transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+        result = await transaction.wait()
+      })
+
+      it('tracts the deposit function', async () =>{
+        expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+        expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount)
+        expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+      })
+
+      it('emits a Deposit event', async () => {
+        const event = result.events[1] // two events are emitted
+        expect(event.event).to.equal('Deposit')
+
+        const args = event.args
+        expect(args.token).to.equal(token1.address)
+        expect(args.user).to.equal(user1.address)
+        expect(args.amount).to.equal(amount)
+        expect(args.balance).to.equal(amount)
+      })
+    })
+
+    describe('Failure', () => {
+      it('fails when no tokens are approved', async() => {
+        //Dont aprove any Tokens
+        await expect(exchange.connect(user1).depositToken(token1.address, amount)).to.be.reverted
+      })
+    })
+
+  })
+})
